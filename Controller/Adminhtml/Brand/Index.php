6d6a633a1d3ddd3706b2677f5d84f5847f282289
Wasim Haider Chowdhury <wasimhc@gmail.com>
2023-09-02 00:08:00 +0600

    ACX-198: Brand grid fixes
    Fix Brand grid errors

--- Controller/Adminhtml/Brand/Index.php
+++ Controller/Adminhtml/Brand/Index.php
@@ -4,29 +4,33 @@
  *  Copyright © Agile Codex Ltd. All rights reserved.
  *  License: https://www.agilecodex.com/license-agreement
  */
-
 namespace Acx\BrandSlider\Controller\Adminhtml\Brand;
 
 /**
  * Brand Index action.
  * @author Agile Codex
  */
-class Index extends \Acx\BrandSlider\Controller\Adminhtml\Brand
+use Magento\Backend\App\Action as AppAction;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends AppAction implements HttpGetActionInterface
 {
-    /**
-     * @var \Magento\Framework\View\Result\PageFactory
-     */
+    /** @var PageFactory */
+    private $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        if ($this->getRequest()->getQuery('ajax')) {
-            $resultForward = $this->_resultForwardFactory->create();
-            $resultForward->forward('grid');
+        $page = $this->pageFactory->create();
+        $page->getConfig()->getTitle()->prepend(__('Brand List'));
 
-            return $resultForward;
-        }
-
-        $resultPage = $this->_resultPageFactory->create();
-
-        return $resultPage;
+        return $page;
     }
 }
---
+++ Ui/Component/DataProvider.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright © Agile Codex Ltd. All rights reserved.
+ * @website www.agilecodex.com
+ */
+
+namespace Acx\BrandSlider\Ui\Component;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider as AbstractDataProvider;
+
+class DataProvider extends AbstractDataProvider
+{
+    public function getData()
+    {
+        return parent::getData();
+    }
+
+}
